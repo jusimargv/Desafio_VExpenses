fc9f0f803fc55a57c9fe3898f0d777e16af5c2b9
jusimargv <jusimargv@gmail.com>
2024-10-17 20:39:02 -0300

    Doc_update

--- main_modificado.tf
+++ main_modificado.tf
@@ -1,73 +1,73 @@
-# Provedor AWS configurado para a região us-east-1
+# Provedor AWS
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 4.0"  # Especificando a versão do provedor para evitar problemas de compatibilidade
+  region = "us-east-1"  # Define a região onde os recursos serão criados
 }
 
-# Variável para armazenar o nome do projeto
+# Variável para o nome do projeto
 variable "projeto" {
   description = "Nome do projeto"
   type        = string
-  default     = "VExpenses"
+  default     = "VExpenses"  # Nome padrão do projeto
 }
 
-# Variável para armazenar o nome do candidato
+# Variável para o nome do candidato
 variable "candidato" {
   description = "Nome do candidato"
   type        = string
-  default     = "SeuNome"
+  default     = "SeuNome"  # Nome padrão do candidato
 }
 
-# Gerando uma chave privada RSA para a instância EC2
+# Geração de uma chave privada para a instância EC2
 resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
+  algorithm = "RSA"   # Algoritmo de criptografia
+  rsa_bits  = 2048    # Tamanho da chave em bits
 }
 
-# Criando um par de chaves no AWS com a chave pública gerada
+# Criação de um par de chaves SSH
 resource "aws_key_pair" "ec2_key_pair" {
   key_name   = "${var.projeto}-${var.candidato}-key"  # Nome da chave
   public_key = tls_private_key.ec2_key.public_key_openssh  # Chave pública
 }
 
-# Criando uma VPC (Virtual Private Cloud)
+# Criação de uma VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"  # Intervalo CIDR para a VPC
-  enable_dns_support   = true  # Habilita suporte a DNS
-  enable_dns_hostnames = true  # Habilita nomes de host DNS
+  cidr_block           = "10.0.0.0/16"  # Bloco CIDR da VPC
+  enable_dns_support   = true            # Habilitar suporte a DNS
+  enable_dns_hostnames = true            # Habilitar nomes de host DNS
 
   tags = {
     Name = "${var.projeto}-${var.candidato}-vpc"  # Nome da VPC
   }
 }
 
-# Criando uma sub-rede na VPC
+# Criação de uma Sub-rede
 resource "aws_subnet" "main_subnet" {
-  vpc_id            = aws_vpc.main_vpc.id  # Referência à VPC
-  cidr_block        = "10.0.1.0/24"  # Intervalo CIDR para a sub-rede
-  availability_zone = "us-east-1a"  # Zona de disponibilidade
+  vpc_id            = aws_vpc.main_vpc.id  # Referência à VPC criada
+  cidr_block        = "10.0.1.0/24"         # Bloco CIDR da sub-rede
+  availability_zone = "us-east-1a"          # Zona de disponibilidade
 
   tags = {
     Name = "${var.projeto}-${var.candidato}-subnet"  # Nome da sub-rede
   }
 }
 
-# Criando um gateway de internet
+# Criação de um Gateway de Internet
 resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main_vpc.id  # Referência à VPC
+  vpc_id = aws_vpc.main_vpc.id  # Associar o gateway à VPC criada
 
   tags = {
     Name = "${var.projeto}-${var.candidato}-igw"  # Nome do gateway
   }
 }
 
-# Criando uma tabela de rotas para a VPC
+# Criação de uma tabela de rotas
 resource "aws_route_table" "main_route_table" {
-  vpc_id = aws_vpc.main_vpc.id  # Referência à VPC
+  vpc_id = aws_vpc.main_vpc.id  # Associar a tabela à VPC
 
+  # Definindo a rota para a Internet
   route {
-    cidr_block = "0.0.0.0/0"  # Rota padrão para tráfego de saída
-    gateway_id = aws_internet_gateway.main_igw.id  # Referência ao gateway de internet
+    cidr_block = "0.0.0.0/0"  # Rota para todas as IPs
+    gateway_id = aws_internet_gateway.main_igw.id  # Usar o gateway de Internet
   }
 
   tags = {
@@ -75,7 +75,7 @@ resource "aws_route_table" "main_route_table" {
   }
 }
 
-# Associando a sub-rede à tabela de rotas
+# Associação da tabela de rotas à sub-rede
 resource "aws_route_table_association" "main_association" {
   subnet_id      = aws_subnet.main_subnet.id  # Referência à sub-rede
   route_table_id = aws_route_table.main_route_table.id  # Referência à tabela de rotas
@@ -85,95 +85,296 @@ resource "aws_route_table_association" "main_association" {
   }
 }
 
-# Criando um grupo de segurança
+# Criação de um Grupo de Segurança
 resource "aws_security_group" "main_sg" {
   name        = "${var.projeto}-${var.candidato}-sg"  # Nome do grupo de segurança
-  description = "Permitir SSH de IP específico e todo o tráfego de saída"  # Descrição do grupo
-  vpc_id      = aws_vpc.main_vpc.id  # Referência à VPC
+  description = "Permitir SSH de IP específico e tráfego HTTP para o Nginx"  # Descrição do grupo
+  vpc_id      = aws_vpc.main_vpc.id  # Associar o grupo à VPC
 
   # Regras de entrada
   ingress {
-    description      = "Allow SSH from specific IP"  # Descrição da regra
-    from_port        = 22  # Porta de entrada para SSH
-    to_port          = 22  # Porta de saída para SSH
-    protocol         = "tcp"  # Protocolo utilizado
-    cidr_blocks      = [var.allowed_ssh_ip]  # IP específico a permitir
-    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6
+    description      = "Permitir SSH de IP específico"
+    from_port        = 22  # Porta SSH
+    to_port          = 22  # Porta SSH
+    protocol         = "tcp"
+    cidr_blocks      = ["SEU_IP_AQUI/32"]  # Substitua pelo seu IP
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
+  }
+
+  # Permitir tráfego HTTP (80) para o Nginx
+  ingress {
+    description      = "Permitir tráfego HTTP"
+    from_port        = 80  # Porta HTTP
+    to_port          = 80  # Porta HTTP
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # Permitir de qualquer lugar
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
   }
 
   # Regras de saída
   egress {
-    description      = "Allow all outbound traffic"  # Descrição da regra
-    from_port        = 0  # Porta de saída
-    to_port          = 0  # Porta de saída
-    protocol         = "-1"  # Permitir todos os protocolos
-    cidr_blocks      = ["0.0.0.0/0"]  # Permitir todo o tráfego
-    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6
+    description      = "Permitir todo tráfego de saída"
+    from_port        = 0  # Todas as portas
+    to_port          = 0  # Todas as portas
+    protocol         = "-1"  # Todos os protocolos
+    cidr_blocks      = ["0.0.0.0/0"]  # Permitir de qualquer lugar
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
   }
 
   tags = {
-    Name = "${var.projeto}-${var.candidato}-sg"  # Nome do grupo de segurança
+    Name = "${var.projeto}-${var.candidato}-sg"  # Nome do grupo
   }
 }
 
-# Recuperando a AMI mais recente do Debian 12
+# Obtenção da AMI mais recente do Debian 12
 data "aws_ami" "debian12" {
-  most_recent = true  # Obtendo a AMI mais recente
+  most_recent = true  # Obter a mais recente
 
   filter {
-    name   = "name"  # Filtro para o nome da AMI
-    values = ["debian-12-amd64-*"]  # Nome da AMI a ser filtrada
+    name   = "name"
+    values = ["debian-12-amd64-*"]  # Filtrar AMIs pelo nome
   }
 
   filter {
-    name   = "virtualization-type"  # Filtro para o tipo de virtualização
-    values = ["hvm"]  # Tipo de virtualização a ser filtrada
+    name   = "virtualization-type"
+    values = ["hvm"]  # Filtrar AMIs por tipo de virtualização
   }
 
   owners = ["679593333241"]  # ID do proprietário da AMI
 }
 
-# Criando uma instância EC2
+# Criação da instância EC2
 resource "aws_instance" "debian_ec2" {
-  ami             = data.aws_ami.debian12.id  # Referência à AMI do Debian 12
-  instance_type   = "t2.micro"  # Tipo de instância EC2
+  ami             = data.aws_ami.debian12.id  # AMI a ser usada
+  instance_type   = "t2.micro"  # Tipo de instância
   subnet_id       = aws_subnet.main_subnet.id  # Referência à sub-rede
-  key_name        = aws_key_pair.ec2_key_pair.key_name  # Nome da chave para acesso
-  security_groups = [aws_security_group.main_sg.name]  # Referência ao grupo de segurança
+  key_name        = aws_key_pair.ec2_key_pair.key_name  # Chave SSH
+  security_groups = [aws_security_group.main_sg.name]  # Grupo de segurança
 
-  associate_public_ip_address = true  # Associar um IP público à instância
+  associate_public_ip_address = true  # Associar um IP público
 
+  # Configuração do volume root
   root_block_device {
-    volume_size           = 20  # Tamanho do volume EBS
-    volume_type           = "gp2"  # Tipo do volume EBS
+    volume_size           = 20  # Tamanho do volume
+    volume_type           = "gp2"  # Tipo de volume
     delete_on_termination = true  # Deletar o volume ao encerrar a instância
-    encrypted             = true  # Criptografar o volume EBS
   }
 
-  # Script de user_data para instalação automática do Nginx
+  # Script de inicialização para instalar e configurar o Nginx
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update -y  # Atualiza os pacotes
-              apt-get upgrade -y  # Atualiza os pacotes existentes
-              apt-get install nginx -y  # Instala o Nginx
-              systemctl start nginx  # Inicia o Nginx
-              systemctl enable nginx  # Habilita o Nginx para iniciar na inicialização
+              apt-get update -y  # Atualizar pacotes
+              apt-get upgrade -y  # Atualizar pacotes instalados
+              apt-get install nginx -y  # Instalar Nginx
+              systemctl start nginx  # Iniciar o serviço Nginx
+              systemctl enable nginx  # Habilitar Nginx para iniciar na inicialização
               EOF
 
   tags = {
-    Name = "${var.projeto}-${var.candidato}-ec2"  # Nome da instância EC2
+    Name = "${var.projeto}-${var.candidato}-ec2"  # Nome da instância
   }
 }
 
-# Saída da chave privada gerada para acesso à instância EC2
+# Saída da chave privada
 output "private_key" {
-  description = "Chave privada para acessar a instância EC2"
+  description = "Chave privada para acessar a instância EC2"  # Descrição da saída
   value       = tls_private_key.ec2_key.private_key_pem  # Valor da chave privada
-  sensitive   = true  # Marcando como sensível
+  sensitive   = true  # Marcar como sensível para não ser exibida no console
 }
 
-# Saída do endereço IP público da instância EC2
+# Saída do IP público da instância EC2
 output "ec2_public_ip" {
-  description = "Endereço IP público da instância EC2"
-  value       = aws_instance.debian_ec2.public_ip  # Endereço IP público da instância
+  description = "Endereço IP público da instância EC2"  # Descrição### Código Atualizado com Melhorias e Comentários
+
+Aqui está o código com as melhorias implementadas e todos os comentários necessários para garantir que atenda a 100% das especificações:
+
+```hcl
+# Provedor AWS
+provider "aws" {
+  region = "us-east-1"  # Define a região onde os recursos serão criados
+}
+
+# Variável para o nome do projeto
+variable "projeto" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "VExpenses"  # Nome padrão do projeto
+}
+
+# Variável para o nome do candidato
+variable "candidato" {
+  description = "Nome do candidato"
+  type        = string
+  default     = "SeuNome"  # Nome padrão do candidato
+}
+
+# Geração de uma chave privada para a instância EC2
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"   # Algoritmo de criptografia
+  rsa_bits  = 2048    # Tamanho da chave em bits
+}
+
+# Criação de um par de chaves SSH
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "${var.projeto}-${var.candidato}-key"  # Nome da chave
+  public_key = tls_private_key.ec2_key.public_key_openssh  # Chave pública
+}
+
+# Criação de uma VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"  # Bloco CIDR da VPC
+  enable_dns_support   = true            # Habilitar suporte a DNS
+  enable_dns_hostnames = true            # Habilitar nomes de host DNS
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-vpc"  # Nome da VPC
+  }
+}
+
+# Criação de uma Sub-rede
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id  # Referência à VPC criada
+  cidr_block        = "10.0.1.0/24"         # Bloco CIDR da sub-rede
+  availability_zone = "us-east-1a"          # Zona de disponibilidade
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-subnet"  # Nome da sub-rede
+  }
+}
+
+# Criação de um Gateway de Internet
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id  # Associar o gateway à VPC criada
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-igw"  # Nome do gateway
+  }
+}
+
+# Criação de uma tabela de rotas
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id  # Associar a tabela à VPC
+
+  # Definindo a rota para a Internet
+  route {
+    cidr_block = "0.0.0.0/0"  # Rota para todas as IPs
+    gateway_id = aws_internet_gateway.main_igw.id  # Usar o gateway de Internet
+  }
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-route_table"  # Nome da tabela de rotas
+  }
+}
+
+# Associação da tabela de rotas à sub-rede
+resource "aws_route_table_association" "main_association" {
+  subnet_id      = aws_subnet.main_subnet.id  # Referência à sub-rede
+  route_table_id = aws_route_table.main_route_table.id  # Referência à tabela de rotas
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-route_table_association"  # Nome da associação
+  }
+}
+
+# Criação de um Grupo de Segurança
+resource "aws_security_group" "main_sg" {
+  name        = "${var.projeto}-${var.candidato}-sg"  # Nome do grupo de segurança
+  description = "Permitir SSH de IP específico e tráfego HTTP para o Nginx"  # Descrição do grupo
+  vpc_id      = aws_vpc.main_vpc.id  # Associar o grupo à VPC
+
+  # Regras de entrada
+  ingress {
+    description      = "Permitir SSH de IP específico"
+    from_port        = 22  # Porta SSH
+    to_port          = 22  # Porta SSH
+    protocol         = "tcp"
+    cidr_blocks      = ["SEU_IP_AQUI/32"]  # Substitua pelo seu IP
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
+  }
+
+  # Permitir tráfego HTTP (80) para o Nginx
+  ingress {
+    description      = "Permitir tráfego HTTP"
+    from_port        = 80  # Porta HTTP
+    to_port          = 80  # Porta HTTP
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # Permitir de qualquer lugar
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
+  }
+
+  # Regras de saída
+  egress {
+    description      = "Permitir todo tráfego de saída"
+    from_port        = 0  # Todas as portas
+    to_port          = 0  # Todas as portas
+    protocol         = "-1"  # Todos os protocolos
+    cidr_blocks      = ["0.0.0.0/0"]  # Permitir de qualquer lugar
+    ipv6_cidr_blocks = ["::/0"]  # Permitir IPv6 (opcional)
+  }
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-sg"  # Nome do grupo
+  }
+}
+
+# Obtenção da AMI mais recente do Debian 12
+data "aws_ami" "debian12" {
+  most_recent = true  # Obter a mais recente
+
+  filter {
+    name   = "name"
+    values = ["debian-12-amd64-*"]  # Filtrar AMIs pelo nome
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]  # Filtrar AMIs por tipo de virtualização
+  }
+
+  owners = ["679593333241"]  # ID do proprietário da AMI
+}
+
+# Criação da instância EC2
+resource "aws_instance" "debian_ec2" {
+  ami             = data.aws_ami.debian12.id  # AMI a ser usada
+  instance_type   = "t2.micro"  # Tipo de instância
+  subnet_id       = aws_subnet.main_subnet.id  # Referência à sub-rede
+  key_name        = aws_key_pair.ec2_key_pair.key_name  # Chave SSH
+  security_groups = [aws_security_group.main_sg.name]  # Grupo de segurança
+
+  associate_public_ip_address = true  # Associar um IP público
+
+  # Configuração do volume root
+  root_block_device {
+    volume_size           = 20  # Tamanho do volume
+    volume_type           = "gp2"  # Tipo de volume
+    delete_on_termination = true  # Deletar o volume ao encerrar a instância
+  }
+
+  # Script de inicialização para instalar e configurar o Nginx
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y  # Atualizar pacotes
+              apt-get upgrade -y  # Atualizar pacotes instalados
+              apt-get install nginx -y  # Instalar Nginx
+              systemctl start nginx  # Iniciar o serviço Nginx
+              systemctl enable nginx  # Habilitar Nginx para iniciar na inicialização
+              EOF
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}-ec2"  # Nome da instância
+  }
+}
+
+# Saída da chave privada
+output "private_key" {
+  description = "Chave privada para acessar a instância EC2"  # Descrição da saída
+  value       = tls_private_key.ec2_key.private_key_pem  # Valor da chave privada
+  sensitive   = true  # Marcar como sensível para não ser exibida no console
+}
+
+# Saída do IP público da instância EC2
+output "ec2_public_ip" {
+  description = "Endereço IP público da instância EC2"  # Descrição do IP público
+  value       = aws_instance.debian_ec2.public_ip  # Valor do IP público
 }
